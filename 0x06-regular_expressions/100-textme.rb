cded372f47ef3db237d374dbd72e344dfbd112f3
Pop2323 <omar4500455@gmail.com>
2023-10-31 15:13:46 -0700

    My 3rd commit

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -2,4 +2,4 @@
 # For this task, you’ll be taking over Guillaume’s responsibilities: one afternoon,
 # a TextMe VoIP Engineer comes to you and explains she wants
 # to run some statistics on the TextMe app text messages transactions.
-puts ARGV[0].scan(\/[from:{.*?}\]\s\[to:{.*?}\]\s\[flags:{.*?}\]/).join
+puts ARGV[0].scan(/\[from:(.*?)\]\s\[to:(.*?)\]\s\[flags:(.*?)\]/).join(',')
