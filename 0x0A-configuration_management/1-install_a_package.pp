c8d9fdd08c43b23740c353d6b8509b239274e59e
Pop2323 <omar4500455@gmail.com>
2023-12-23 02:10:26 -0800

    updated3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
-# Using Puppet, install flask from pip3
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+}
+
+exec { 'install_python':
+  command => '/usr/bin/pip3 install python==3.8.10',
+}
+
+exec { 'install_werkzeug':
+  command => '/usr/bin/pip3 install Werkzeug==2.1.1',
 }
