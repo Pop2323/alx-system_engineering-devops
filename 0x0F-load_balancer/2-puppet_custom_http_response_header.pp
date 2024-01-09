08e99dfaf93c691093bb1971f1a733c8ba057f77
Pop2323 <omar4500455@gmail.com>
2024-01-09 12:36:45 -0800

    loadblancer_4

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,14 +1,19 @@
-exec {'update':
-  command => '/usr/bin/apt-get update',
+#puppet advance
+exec { 'update':
+  command  => 'sudo apt-get update',
+  provider => shell,
 }
 -> package {'nginx':
-  ensure => 'present',
+  ensure => present,
 }
--> file_line { 'http_header':
-  path  => '/etc/nginx/nginx.conf',
-  match => 'http {',
-  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
+-> file_line { 'header line':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  line   => "	location / {
+  add_header X-Served-By ${hostname};",
+  match  => '^\tlocation / {',
 }
--> exec {'run':
-  command => '/usr/sbin/service nginx restart',
+-> exec { 'restart service':
+  command  => 'sudo service nginx restart',
+  provider => shell,
 }
