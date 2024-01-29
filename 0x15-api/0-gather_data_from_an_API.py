5d0c92ef5937983030f873db80f7249a7cf4e7bc
Pop2323 <omar4500455@gmail.com>
2024-01-29 13:36:04 -0800

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-# Write a Python script that, using this REST API, for a given
-# employee ID, returns information about his/her Todo list progress.
+"""Write a Python script that, using this REST API, for a given
+employee ID, returns information about his/her Todo list progress."""
 
 import requests
 import sys
@@ -14,24 +14,19 @@ if __name__ == "__main__":
     jsonplaceholder = 'https://jsonplaceholder.typicode.com/users'
     url = f'{jsonplaceholder}/{employee_ID}'
 
-    # Make a GET request to the API
     response = requests.get(url)
 
-    # Check if the request was successful (status code 200)
     if response.status_code == 200:
         employee_name = response.json().get('name')
         Todourl = f'{url}/todos'
         res = requests.get(Todourl)
         tasks = res.json()
 
-        # Filter completed tasks
         done_tasks = [task for task in tasks if task.get('completed')]
 
-        # Display the employee TODO list progress
         print("Employee {} is done with tasks({}/{}):".format(
             employee_name, len(done_tasks), len(tasks)))
         for task in done_tasks:
             print("\t{}".format(task.get('title')))
     else:
-        # Display an error message if the request was not successful
         print(f"Error: Status code: {response.status_code}")
--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-# Using what you did in the task #0, extend your Python
-# script to export data in the JSON format.
+"""Using what you did in the task #0, extend your Python
+script to export data in the JSON format."""
 
 import json
 import requests
 import sys
+
 
 if __name__ == "__main__":
     jsonplaceholder = 'https://jsonplaceholder.typicode.com/users'
@@ -28,7 +29,5 @@ if __name__ == "__main__":
                 "task": task.get("title"),
                 "completed": task.get("completed"),
             })
-
-    # Write the tasks data to a JSON file
-    with open('todo_all_employees.json', 'w') as f:
-        json.dump(data_dict, f)
+            with open('todo_all_employees.json', 'w') as f:
+                json.dump(data_dict, f)
