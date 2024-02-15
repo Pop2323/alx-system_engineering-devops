53550fb757a7d6b55b18cdfd2463a88161656fe0
Pop2323 <omar4500455@gmail.com>
2024-02-15 14:57:32 -0800

    API-Advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,8 +5,8 @@ import sys
 
 
 def number_of_subscribers(subreddit):
-    """function that queries the Reddit API and returns
-    the number of subscribers"""
+    """ function that queries the Reddit API and returns
+        the number of subscribers"""
     headers = {'User-Agent': 'Custom'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     response = requests.get(url, headers=headers, allow_redirects=False)
