b29aeaedf45b2dabb02909b705a6530ddc56a23b
Pop2323 <omar4500455@gmail.com>
2024-02-15 15:58:48 -0800

    API-Advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""function that queries the Reddit API"""
+""" Function that queries the Reddit API """
 import requests
 import sys
 
 
 def number_of_subscribers(subreddit):
-    """ function that queries the Reddit API and returns
-        the number of subscribers"""
+    """function that queries the Reddit API and prints
+        the titles of the first 10 hot posts
+        listed for a given subreddit."""
     headers = {'User-Agent': 'Custom'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
-        return (response.json().get('data').get('subscribers'))
+        return (response.json().get("data").get("subscribers"))
     return (0)
