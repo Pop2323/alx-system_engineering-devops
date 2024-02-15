2af8107baba42090228b1c3f59a20f667838371a
Pop2323 <omar4500455@gmail.com>
2024-02-15 14:53:48 -0800

    API-Advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
-
+"""function that queries the Reddit API"""
 import requests
 import sys
-
-
-"""Write a function that queries the Reddit API and returns
-the number of subscribers
-(not active users, total subscribers) for a given subreddit.
-If an invalid subreddit is given, the function should return 0."""
 
 
 def number_of_subscribers(subreddit):
